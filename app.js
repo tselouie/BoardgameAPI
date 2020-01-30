19b8d8e3b05bac85699aa4de07809cb114581659
Zenna Liang <zenna.liang@gmail.com>
2020-01-30 17:04:25 -0500

    added user signin with token

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
+var cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -28,6 +29,7 @@ const authRoutes = require("./routes/auth");
 // middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(expressValidator());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
-//const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken"); // maintain user status on web
 require("dotenv").config();
+
 const User = require("../models/user");
 
 exports.signUp = async (req, res) => {
@@ -14,29 +15,29 @@ exports.signUp = async (req, res) => {
     res.status(200).json({ message: "Signup success! Please login." });
 };
 
-// exports.signIn = (req, res) => {
-//     // find the user based on email
-//     const { email, password } = req.body;
-//     User.findOne({ email }, (err, user) => {
-//         // if err or no user
-//         if (err || !user) {
-//             return res.status(401).json({
-//                 error: "User with that email does not exist. Please signin."
-//             });
-//         }
-//         // if user is found make sure the email and password match
-//         // create authenticate method in model and use here
-//         if (!user.authenticate(password)) {
-//             return res.status(401).json({
-//                 error: "Email and password do not match"
-//             });
-//         }
-//         // generate a token with user id and secret
-//         const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-//         // persist the token as 't' in cookie with expiry date
-//         res.cookie("t", token, { expire: new Date() + 9999 });
-//         // retrun response with user and token to frontend client
-//         const { _id, name, email } = user;
-//         return res.json({ token, user: { _id, email, name } });
-//     });
-// };
+exports.signIn = (req, res) => {
+    // find the user based on email
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+        // if err or no user
+        if (err || !user) {
+            return res.status(401).json({
+                error: "User with that email does not exist. Please signin."
+            });
+        }
+        // if user is found make sure the email and password match
+        // create authenticate method in model and use here
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password do not match"
+            });
+        }
+        // generate a token with user id and secret
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        // persist the token as 't' in cookie with expiry date
+        res.cookie("t", token, { expire: new Date() + 9999 });
+        // retrun response with user and token to frontend client
+        const { _id, name, email } = user;
+        return res.json({ token, user: { _id, email, name } });
+    });
+};
